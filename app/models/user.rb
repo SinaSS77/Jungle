33d0c4fc6d1cbe8af41d4fac5423043f01b19a1f
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-03-09 17:51:47 -0800

    Add Admin Category and User Authentication

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 5 }
+  before_save :normalize_email
+
+  def normalize_email
+    self.email = self.email.downcase.strip
+  end
+
+  # methods the work on classes need to reference self
+  def self.authenticate_with_credentials(email, password)
+    clean_email = email.strip.downcase
+    user = User.find_by_email(clean_email)
+    if user && user.authenticate(password)
+      user
+    else 
+      nil
+    end
+  end
+end
